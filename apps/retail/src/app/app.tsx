cab071ec9806ca58edc83f515c4806fdbba652fa
tambEW <itsmetambui@gmail.com>
2021-09-13 21:11:21 +0700

    add partner app

--- apps/retail/src/app/app.tsx
+++ apps/retail/src/app/app.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { DashboardPage } from '../pages/dashboard';
 import { SigninPage } from '../pages/signin';
 
 export const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <DashboardPage />
-        </Route>
-        <Route path="/signin">
-          <SigninPage />
-        </Route>
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/">
+        <DashboardPage />
+      </Route>
+      <Route path="/signin">
+        <SigninPage />
+      </Route>
+    </Switch>
   );
 };
